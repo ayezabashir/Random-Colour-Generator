4a647daf91bd9ad2ae34a2c9ed30ab87c5d5c5d6
Ayeza <ayezabashir442@gmail.com>
2023-04-10 00:29:29 +0500

    final commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const hexCode = document.querySelector(".hex-color");
+const btn = document.getElementById("btn");
+
+// initial render to display a random color by default
+generateColor();
+
+function generateColor() {
+    let randNum = Math.floor(Math.random() * 16777215);
+    let colorCode = "#" + randNum.toString(16); //Convert a randNum to a string, using base 16 (Hexadecimal)
+    document.body.style.backgroundColor = colorCode;
+    hexCode.textContent = colorCode;
+}
+
+btn.addEventListener('click', generateColor);
